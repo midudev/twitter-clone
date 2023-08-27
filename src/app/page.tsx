20925266e636ef617bdc9be2b5aa63ab73233164
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-08-27 02:06:21 -0400

    fix(types): resolve type discrepancy in PostList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,39 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { AuthButtonServer } from '@/app/components/auth-button-server'
-import { redirect } from 'next/navigation'
-import { PostLists } from './components/posts-list'
-import { type Database } from './types/database'
-import { ComposePost } from './components/compose-post'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { AuthButtonServer } from '@/app/components/auth-button-server';
+import { redirect } from 'next/navigation';
+import { PostLists } from './components/posts-list';
+import { type Database } from './types/database';
+import { ComposePost } from './components/compose-post';
 
-export default async function Home () {
-  const supabase = createServerComponentClient<Database>({ cookies })
-  const { data: { session } } = await supabase.auth.getSession()
+export default async function Home() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
   if (session === null) {
-    redirect('/login')
+    redirect('/login');
   }
 
-  const { data: posts } = await supabase
+  const { data } = await supabase
     .from('posts')
     .select('*, user:users(name, avatar_url, user_name)')
-    .order('created_at', { ascending: false })
+    .order('created_at', { ascending: false });
+
+  const posts =
+    data?.map((post) => ({
+      ...post,
+      user: Array.isArray(post.user) ? post.user[0] : post.user,
+    })) ?? [];
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-
       <section className="max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen">
         <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
         <PostLists posts={posts} />
       </section>
       <AuthButtonServer />
     </main>
-  )
+  );
 }
